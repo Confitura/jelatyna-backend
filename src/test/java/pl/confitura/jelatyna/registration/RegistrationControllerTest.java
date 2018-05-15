fd7fbf1dc3556d305a095c07ea7f33ea9b4661b7
dwiernicki <D.Wiernicki@polish-airports.com>
2018-05-15 18:45:36 +0200

    prepare for development

---
+++ src/test/java/pl/confitura/jelatyna/registration/RegistrationControllerTest.java
@@ -0,0 +1,18 @@
+package pl.confitura.jelatyna.registration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationControllerTest {
+
+    @Test
+    void shouldAcceptValidToken(){
+        fail("shouldNotAcceptInvalidToken");
+    }
+
+    @Test
+    void shouldNotAcceptInvalidToken(){
+        fail("shouldNotAcceptInvalidToken");
+    }
+}
